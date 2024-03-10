3c34f581debd123ff7ad3f7b0d3bec9e4e460821
Saifiboi <saifurrehman1357@gmail.com>
2024-03-10 19:38:23 +0500

    .

--- Semester1/PF_lab/practice/q42.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int num;
-	cout<<"\nEnter the number:";
-	cin>>num;
-	(num>0)?(num<18)?cout<<"\nHe can't vote\n":cout<<"\nHe can vote\n":cout<<"\nInvalid Input\n";
-}
